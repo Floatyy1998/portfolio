c439d7b96d5cec1d9a2bf89c0dedd9c41f65edb8
Floatyy1998 <you@example.com>
2023-10-16 00:00:33 +0200

    added Avatar

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,19 +1,40 @@
 import "./Home.css";
 import Typewriter from "typewriter-effect";
+import Avatar from "@mui/material/Avatar";
+import profilbilild from "../../assets/Profilbild1.jpg";
 const Home = () => {
   return (
     <div className="home-container">
-        <h1>Hi,<br/> I'm <span className="name">Konrad</span>, a</h1>
-     <Typewriter
-              options={{
-              strings: ['Fullstack Developer','Web Developer', 'Web Designer', 'React Developer'],
-              autoStart: true,
-              loop: true,
-              deleteSpeed:50,
-              delay:50
-              
-              }}
-            />
+      <Avatar
+        alt="Remy Sharp"
+        src={profilbilild}
+        sx={{
+          width: "20vh",
+          height: "20vh",
+          border: "2px solid #00fed7",
+          boxShadow:
+            "rgba(0,212,255, 0.5) 12px 11px 20px 0px, rgba(0,212,255, 0.5) -5px -5px 20px 0px",
+            marginTop: "2vh"
+        }}
+        className="avatar"
+      />
+      <h1>
+        Hi, I'm <span className="name">Konrad</span>, a
+      </h1>
+      <Typewriter
+        options={{
+          strings: [
+            "Fullstack Developer",
+            "Web Developer",
+            "Web Designer",
+            "React Developer",
+          ],
+          autoStart: true,
+          loop: true,
+          deleteSpeed: 50,
+          delay: 50,
+        }}
+      />
     </div>
   );
 };
